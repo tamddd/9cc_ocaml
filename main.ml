06e5d844fa8e5664f0f6de625eb37421d1cbd748
tamura <daiki1106tamu@gmail.com>
2024-03-04 20:56:11 +0900

    空白なしなら動く

--- main.ml
+++ main.ml
@@ -11,37 +11,36 @@ let main argv =
       if pointer < (String.length s) then
           if (s.[pointer] = '+') then
             let number =  (String.sub s num_pointer (pointer - num_pointer)) in
-            print_endline number;
             (strtol (pointer + 1) (pointer + 1) s) ((lst @ [number]) @ ["add"])
             
           else if s.[pointer] = '-' then
             let number =  (String.sub s num_pointer (pointer - num_pointer)) in
             (strtol (pointer + 1) (pointer + 1) s) (lst @ [number] @ ["sub"])
           else if s.[pointer] = ' ' then
-            (*数字があったのに空白になる その場合は数字ポインタは進めない
-                逆にまだ数字が始まってない場合は進めていい
-                *)
                 if num_pointer = pointer then
                   strtol (pointer + 1) (num_pointer + 1) s lst
                 else strtol (pointer + 1) (num_pointer) s lst
+
           else strtol (pointer + 1) num_pointer s  lst
-      else 
-          (lst);;
+      else
+        let number = (String.sub s num_pointer (pointer - num_pointer)) in
+            (lst @ [number]);;
     (*
     print_endline ".intel_syntax noprefix";
     print_endline ".globl main";
     print_endline "main:";
     print_endline ("  mov rax, " ^ string_of_int (int_of_string Sys.argv.(1)));
     *)
+    
+    
     let empty_list = [] in
     let ans = strtol 0 0 p empty_list in
     let rec print_lst lst = match lst with 
     [] -> ()
     | first :: rest -> begin 
-      print_string (first ^ " ");
+      print_string (first ^ " \n");
       print_lst rest
     end in print_lst ans;
-    
     print_endline "  ret";;
 
 let () = main()
